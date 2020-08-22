18d86abc5daa30b0ead5aef9c1a7f0658c7ef92a
beckyphan <github email address>
2020-08-22 14:51:21 -0700

    wrapped all inside DOMContentLoaded, created Registration form

--- src/index.js
+++ src/index.js
@@ -9,38 +9,66 @@ document.addEventListener('DOMContentLoaded', () => {
     showRegisterOrLogin()
   })
 
+
+
+  function showRegisterOrLogin() {
+    nav.innerHTML = `
+      <button id="loginButton">Log In</button>
+      <button id="registerButton">Register</button>
+      `
+
+    const loginButton = document.querySelector('#loginButton')
+    const registerButton = document.querySelector('#registerButton')
+
+    loginButton.addEventListener("click", (e) => {
+      showLoginForm()
+    })
+
+    registerButton.addEventListener("click", (e) => {
+      showRegisterForm()
+    })
+  }
+
+  function showLoginForm() {
+    nav.innerHTML = `
+      <div id="login">
+        <form class="login-form">
+          <input type="text" name="email" value="" placeholder="email" class="input-text"/>
+          <input type="text" name="password" value="" placeholder="password" class="input-text"/>
+          <input type="submit" name="submit" value="Submit" class="submit" />
+        </form>
+      </div>
+      `
+  }
+
+  function showRegisterForm() {
+    nav.innerHTML = `
+      <div id="register">
+        <h2>Register New User</h2>
+        <form class="login-form">
+          <label for="[user]name">Name:</label>
+          <input type="text" name="[user]name" value="" placeholder="name" class="input-text"/>
+          <label for="[user]email">E-mail:</label>
+          <input type="text" name="[user]email" value="" placeholder="email" class="input-text"/>
+          </br>
+
+          <label for="[user]carb_grams">Daily Carbs (g) Target:</label>
+          <input type="text" name="[user]carb_grams" value="" placeholder="300" class="input-text"/></br>
+
+          <label for="[user]protein_grams">Daily Protein (g) Target:</label>
+          <input type="text" name="[user]protein_grams" value="" placeholder="50" class="input-text"/></br>
+
+          <label for="[user]fat_grams">Daily Fat (g) Target:</label>
+          <input type="text" name="[user]fat_grams" value="" placeholder="60" class="input-text"/></br>
+
+          <label for="[user]password">Password:</label>
+          <input type="password" name="[user]password" value="" placeholder="password" class="input-text"/>
+          </br>
+
+          <input type="submit" name="submit" value="Submit" class="submit" />
+        </form>
+      </div>
+      `
+  }
+
 })
-
-function showRegisterOrLogin() {
-  nav.innerHTML = `
-    <button id="loginButton">Log In</button>
-    <button id="registerButton">Register</button>
-    `
-
-  const loginButton = document.querySelector('#loginButton')
-  const registerButton = document.querySelector('#registerButton')
-
-  loginButton.addEventListener("click", (e) => {
-    showLoginForm()
-  })
-
-  registerButton.addEventListener("click", (e) => {
-    showRegisterForm()
-  })
-}
-
-function showLoginForm() {
-  nav.innerHTML = `
-    <div id="login">
-      <form class="login-form">
-        <input type="text" name="email" value="" placeholder="email" class="input-text"/>
-        <input type="text" name="password" value="" placeholder="password" class="input-text"/>
-        <input type="submit" name="submit" value="Submit" class="submit" />
-      </form>
-    </div>
-    `
-}
-
-function showRegisterForm() {
-
-}
