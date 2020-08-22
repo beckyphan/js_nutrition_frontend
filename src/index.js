f6b21d8a2f27e9228816f593616b4e6702508ba8
beckyphan <github email address>
2020-08-22 14:36:52 -0700

    able to show login form when click on login from nav bar

--- src/index.js
+++ src/index.js
@@ -16,4 +16,26 @@ function showRegisterOrLogin() {
     <button id="loginButton">Log In</button>
     <button id="registerButton">Register</button>
     `
+
+  const loginButton = document.querySelector('#loginButton')
+  const registerButton = document.querySelector('#registerButton')
+
+  loginButton.addEventListener("click", (e) => {
+    showLoginForm()
+  })
+}
+
+function showLoginForm() {
+  console.log("showLoginForm")
+  const nav = document.querySelector('nav')
+
+  nav.innerHTML = `
+    <div id="login">
+      <form class="login-form">
+        <input type="text" name="email" value="" placeholder="email" class="input-text"/>
+        <input type="text" name="password" value="" placeholder="password" class="input-text"/>
+        <input type="submit" name="submit" value="Submit" class="submit" />
+      </form>
+    </div>
+    `
 }
