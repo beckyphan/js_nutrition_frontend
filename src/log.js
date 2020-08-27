73af54ab6ead8eacb9ab39c5a71534016bf0956c
beckyphan <github email address>
2020-08-26 21:19:59 -0700

    added log class properties to be able to track nutrition sums

--- src/log.js
+++ src/log.js
@@ -3,11 +3,44 @@ class Log {
   constructor(log, logAttributes, logRelationships) {
     this.id = log.id
     this.caldate = logAttributes.caldate
-    this.loggedFoodsArray = logRelationships.data
-    this.logFoodsIds = []
-    for (let i = 0; i < this.loggedFoodsArray.length; i++) {
-      this.logFoodsIds.push(this.loggedFoodsArray[i].id)
+    this._loggedFoodsArray = logRelationships.data
+    this._logFoodsIds = []
+    for (let i = 0; i < this._loggedFoodsArray.length; i++) {
+      this._logFoodsIds.push(this._loggedFoodsArray[i].id)
     }
+    this.logCarb = []
+    this.logProtein = []
+    this.logFat = []
+  }
+
+  addNutrientsToLog(carb, protein, fat) {
+    this.logCarb.push(carb)
+    this.logProtein.push(protein)
+    this.logFat.push(fat)
+  }
+
+  totalCarb() {
+    let total = 0;
+    for (element of this.logCarb) {
+      total += element
+    }
+    return total
+  }
+
+  totalProtein() {
+    let total = 0;
+    for (element of this.logProtein) {
+      total += element
+    }
+    return total
+  }
+
+  totalFat() {
+    let total = 0;
+    for (element of this.logFat) {
+      total += element
+    }
+    return total
   }
 
   renderLogDate() {
@@ -16,8 +49,8 @@ class Log {
 
   renderLoggedFoods() {
     let loggedFoods = ""
-    for (let i = 0; i < this.logFoodsIds.length; i++) {
-      fetchFoodLogProperties(this.logFoodsIds[i])
+    for (let i = 0; i < this._logFoodsIds.length; i++) {
+      fetchFoodLogProperties(this._logFoodsIds[i])
     }
 
     return loggedFoods
